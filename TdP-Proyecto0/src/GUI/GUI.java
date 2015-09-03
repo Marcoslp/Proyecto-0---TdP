b465794166cb087017cfa87fc63c72afc2304518
Poldoroski <Poldoroski@Poldoroski-PC>
2015-09-02 23:26:48 -0300

    Listo.

---
+++ TdP-Proyecto0/src/GUI/GUI.java
@@ -0,0 +1,63 @@
+package GUI;
+
+import java.awt.EventQueue;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GUI {
+
+	private JFrame frame;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	public GUI() {
+		initialize();
+	}
+
+	
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 379, 465);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel Imagen = new JLabel();
+		Imagen.setBounds(86, 15, 181, 285);
+		frame.getContentPane().add(Imagen);
+		Imagen.setIcon(new ImageIcon(getClass().getResource("/Imagenes/SeñorTopo.jpg")));
+		
+		JLabel Frase = new JLabel("");
+		Frase.setBounds(40, 311, 313, 30);
+		frame.getContentPane().add(Frase);
+		
+		JButton Boton = new JButton("Ver frase");
+		Boton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Frase.setText("Aprobar TDP es como comer una naranja");
+				Imagen.setIcon(new ImageIcon(getClass().getResource("/Imagenes/SeñorTopo2.jpg")));
+				Boton.setEnabled(false);
+			}
+		});
+		Boton.setBounds(96, 352, 161, 37);
+		frame.getContentPane().add(Boton);
+	}
+}
