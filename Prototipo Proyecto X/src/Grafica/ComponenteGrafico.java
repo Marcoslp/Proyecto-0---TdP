689342ef6c82ed675e4d01973088cbb5a36e4678
Poldoroski <Poldoroski@Poldoroski-PC>
2015-10-20 20:38:20 -0300

    Ji

---
+++ Prototipo Proyecto X/src/Grafica/ComponenteGrafico.java
@@ -0,0 +1,33 @@
+package Grafica;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+public class ComponenteGrafico {
+	protected JLabel imagenActual;
+	protected Icon misImagenes[];
+	protected final int ALTO  = 32;
+	protected final int ANCHO = 32;
+	
+	public ComponenteGrafico(int i){
+		misImagenes = new Icon[i];
+		imagenActual = new JLabel();
+	}
+	public void establecerimagenActual(int i){
+		imagenActual.setIcon(misImagenes[i]);
+	}
+	
+	public void establecerImagen(Icon j, int i){
+		misImagenes[i] = j;
+	}
+	
+	public JLabel obtenerImagenActual(){
+		return imagenActual;
+	}
+	public int obtenerAncho() {
+		return ANCHO;
+	}
+	public int obtenerAlto() {
+		return ALTO;
+	}
+}
