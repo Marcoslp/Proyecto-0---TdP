7e234611ce76d2c89080ff7146f03b5db344d275
Marcos Leguizamón <marcos.leguizamonlp@hotmail.com>
2015-10-23 15:21:54 -0300

    Sprites acrualizados

--- Prototipo Proyecto X/src/Grafica/AltairGrafico.java
+++ Prototipo Proyecto X/src/Grafica/AltairGrafico.java
@@ -12,7 +12,7 @@ public class AltairGrafico extends ComponenteGrafico{
 	public AltairGrafico(int i,int x,int y) {
 		super(i);
 		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/FichinIzquierda.gif")), 0);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/FichinIzquierda.gif")), 1);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/FichinDerecha.gif")), 1);
 		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/FichinIzquierda.gif")), 2);
 		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/FichinIzquierda.gif")), 3);
 		this.establecerimagenActual(3);
