3c77e8e9c942e3fdb1dee3a6209c816a260b0283
Marcos Leguizamón <marcos.leguizamonlp@hotmail.com>
2015-10-23 15:12:32 -0300

    Sprite de Altair cambiado, Hernán fijate la clase AltairGrafica ahí esta hechoe el javadoc

--- Prototipo Proyecto X/src/Grafica/AltairGrafico.java
+++ Prototipo Proyecto X/src/Grafica/AltairGrafico.java
@@ -2,14 +2,19 @@ package Grafica;
 
 import javax.swing.ImageIcon;
 
+/**
+ * Clase que representa la gráfica del Altair
+ * @author Usuario
+ *
+ */
 public class AltairGrafico extends ComponenteGrafico{
 
 	public AltairGrafico(int i,int x,int y) {
 		super(i);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/ricky.jpg")), 0);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/ricky.jpg")), 1);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/ricky.jpg")), 2);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/ricky.jpg")), 3);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/FichinIzquierda.gif")), 0);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/FichinIzquierda.gif")), 1);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/FichinIzquierda.gif")), 2);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/FichinIzquierda.gif")), 3);
 		this.establecerimagenActual(3);
 		this.obtenerImagenActual().setBounds(x*32, y*32, ALTO, ANCHO);
 	}
