ec3912302a6757d7b0d25b459d5605c0bb1453ff
Marcos Leguizamón <marcos.leguizamonlp@hotmail.com>
2015-10-30 20:32:28 -0300

    Sprites actualizados!

--- Prototipo Proyecto X/src/Grafica/AltairGrafico.java
+++ Prototipo Proyecto X/src/Grafica/AltairGrafico.java
@@ -13,8 +13,8 @@ public class AltairGrafico extends ComponenteGrafico{
 		super(i);
 		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/altair-izquierda.gif")), 0);
 		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/altair-derecha.gif")), 1);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/rogulo-arriba.png")), 2);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/rogulo-abajo.png")), 3);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/altair-arriba.png")), 2);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/altair-abajo.png")), 3);
 		this.establecerimagenActual(3);
 		this.obtenerImagenActual().setBounds(x*32, y*32, ALTO, ANCHO);
 	}
--- Prototipo Proyecto X/src/Grafica/BombermanGrafico.java
+++ Prototipo Proyecto X/src/Grafica/BombermanGrafico.java
@@ -12,11 +12,11 @@ public class BombermanGrafico extends ComponenteGrafico {
 
 	public BombermanGrafico(int i, int x, int y) {
 		super(i);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/B-izquierda.png")), 0);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/B-derecha.png")), 1);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/B-izquierda.gif")), 0);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/B-derecha.gif")), 1);
 		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/B-arriba.gif")), 2);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/B-abajo.png")), 3);
-		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/B-arribaStatico.png")), 4);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/B-abajo.gif")), 3);
+		this.establecerImagen(new ImageIcon(this.getClass().getResource("/Imagenes/b-centro1.png")), 4);
 		this.establecerimagenActual(3);
 		this.obtenerImagenActual().setBounds(x*32, y*32, ANCHO,ALTO);
 	}
